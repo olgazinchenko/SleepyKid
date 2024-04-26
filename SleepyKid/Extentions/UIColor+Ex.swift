a4afa9c971b8d3da3bee955802970a2ca74f2ea1
olgazinchenko <ozinchenko.pm@gmail.com>
2024-04-26 10:59:16 +0200

    Added UIColor extension and main colours in the app

---
+++ SleepyKid/Extentions/UIColor+Ex.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Ex.swift
+//  SleepyKid
+//
+//  Created by ozinchenko.dev on 26/04/2024.
+//
+
+import UIKit
+
+extension UIColor {
+    static let mainPink = UIColor(red: 1.00, green: 0.46, blue: 0.81, alpha: 1.00)
+    static let mainYellow = UIColor(red: 0.99, green: 1.00, blue: 0.76, alpha: 1.00)
+    static let mainMint = UIColor(red: 0.58, green: 1.00, blue: 0.85, alpha: 1.00)
+    static let mainBlue = UIColor(red: 0.64, green: 0.85, blue: 1.00, alpha: 1.00)
+}
